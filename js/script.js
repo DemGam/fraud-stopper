8d4bbc1b12dcbd9e2929a5d254cc8e59abe84805
Alex Demyan <107802234+DemGam@users.noreply.github.com>
2025-05-31 21:19:54 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,52 @@
+"use strict";
+
+window.addEventListener("load", function () {
+  var isScrolling;
+  var position = 0;
+  var delta = 5;
+  var header =
+    document.querySelector(".header") || document.querySelector("#top-menu");
+  var stickySidebar = document.querySelector(".sidebar__sticky-content");
+  var docHeight = document.documentElement.scrollHeight;
+  var winHeight = window.innerHeight;
+
+  window.addEventListener("scroll", function (event) {
+    isScrolling = true;
+  });
+
+  setInterval(function () {
+    if (isScrolling && header) {
+      scrollFunction();
+      isScrolling = false;
+    }
+  }, 250);
+
+  function scrollFunction() {
+    var currentScrollPosition =
+      window.pageYOffset || document.documentElement.scrollTop;
+
+    if (Math.abs(position - currentScrollPosition) <= delta) return;
+
+    if (
+      currentScrollPosition > position &&
+      currentScrollPosition > header.offsetHeight
+    ) {
+      header.classList.add("header-hidden");
+      if (stickySidebar) {
+        stickySidebar.classList.add("moveUp");
+      }
+    } else {
+      if (currentScrollPosition + winHeight < docHeight) {
+        header.classList.remove("header-hidden");
+        if (stickySidebar) {
+          stickySidebar.classList.remove("moveUp");
+        }
+      }
+    }
+    position = currentScrollPosition;
+  }
+});
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+};
